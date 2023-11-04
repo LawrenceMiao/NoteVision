d017c680135d825696fbae76e44b6e851a0f93c3
Sherry Lin <lins11@rpi.edu>
2023-11-04 11:27:27 -0400

    Python file handling test code update

--- docs/sherry-docs/learning code/file_handling.py
+++ docs/sherry-docs/learning code/file_handling.py
@@ -9,31 +9,31 @@ def readLineByLine(file):
     print(line)
 
 if __name__ == '__main__':
-  test_file1 = openFile("/Users/sherrylin/Library/CloudStorage/Dropbox/cs2961/NoteVision/docs/sherry-docs/learning code/one_quote1.txt", "rt")
+  test_file1 = openFile("one_quote1.txt", "rt")
   # print(test_file1.read())
   readLineByLine(test_file1)
   closeFile(test_file1)
 
-  test_file2 = openFile("/Users/sherrylin/Library/CloudStorage/Dropbox/cs2961/NoteVision/docs/sherry-docs/learning code/one_quote2.txt", "a")
+  test_file2 = openFile("one_quote2.txt", "a")
   test_file2.write("\n comment: interesting quote")
   closeFile(test_file2)
 
-  test_file2 = openFile("/Users/sherrylin/Library/CloudStorage/Dropbox/cs2961/NoteVision/docs/sherry-docs/learning code/one_quote2.txt", "r")
+  test_file2 = openFile("one_quote2.txt", "r")
   readLineByLine(test_file2)
   closeFile(test_file2)
 
-  test_file3 = openFile("/Users/sherrylin/Library/CloudStorage/Dropbox/cs2961/NoteVision/docs/sherry-docs/learning code/test_image.png", "rb")
+  test_file3 = openFile("test_image.png", "rb")
   # print(test_file3.read())
   closeFile(test_file3)
 
-  test_file4 = openFile("/Users/sherrylin/Library/CloudStorage/Dropbox/cs2961/NoteVision/docs/sherry-docs/learning code/test.txt", "x")
+  test_file4 = openFile("test.txt", "x")
   closeFile(test_file4)
 
-  test_file5 = openFile("/Users/sherrylin/Library/CloudStorage/Dropbox/cs2961/NoteVision/docs/sherry-docs/learning code/created_quote.txt", "w")
+  test_file5 = openFile("created_quote.txt", "w")
   test_file5.write("some inspirational stuff - inspirational person")
   closeFile(test_file5)
 
-  test_file6 = openFile("/Users/sherrylin/Library/CloudStorage/Dropbox/cs2961/NoteVision/docs/sherry-docs/learning code/two_quotes.txt", "r")
+  test_file6 = openFile("two_quotes.txt", "r")
   # print(test_file6.readline())
   readLineByLine(test_file6)
   closeFile(test_file6)
