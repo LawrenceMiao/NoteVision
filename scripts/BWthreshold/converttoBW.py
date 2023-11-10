a83b19422820f8d7220ba593b525447e314951d9
Sherry Lin <lins11@rpi.edu>
2023-11-10 08:23:43 -0500

    edit converttoBW.py

--- scripts/BWthreshold/converttoBW.py
+++ scripts/BWthreshold/converttoBW.py
@@ -1,30 +1,21 @@
 '''
-input a PNG or JPEG file
-converts color image to greyscale
-apply black and white thresholding to the image
-save it as a new PNG
+input: PNG or JPEG file
+output: black and white threshold PNG image
 '''
 import sys, cv2
 
-def bw_filter(input_img, output_img):
-  color_img = cv2.imread(input_img)
-  grey_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
-  (thresh, bw_img) = cv2.threshold(grey_img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+def bw_filter(input_img):
+  grey_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+  bw_img = cv2.threshold(grey_img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-  cv2.imwrite(output_img, bw_img)
-
-  # cv2.imshow('Color Image', color_img)
-  # cv2.imshow('Grayscale Image', grey_img)
-  # cv2.imshow('Black and White Image', bw_img)
-  # cv2.waitKey(0)
-  # cv2.destroyAllWindows()
+  return bw_img[1]
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
     print("No input file given.")
     exit()
-  else:
-    input_img = sys.argv[1]
+    
+  input_img = sys.argv[1]
     
   file_type = "." + input_img.rsplit('.', 1)[1]
 
@@ -35,7 +26,8 @@ if __name__ == "__main__":
   else:
     output_img = input_img.replace(file_type,"") + "_bw.png"
 
-  bw_filter(input_img, output_img)
+  bw_img = bw_filter(cv2.imread(input_img))
+  cv2.imwrite(output_img, bw_img)
 
   print(f"Applied black and white thresholding to {input_img}.")
   print(f"Input file: {input_img}")
