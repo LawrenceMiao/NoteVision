b667d503c71d8428439824023b0db0781c27f99c
williamw04 <williamwu120@gmail.com>
2023-11-24 02:49:02 -0500

    better scrolling animation

--- web/script.js
+++ web/script.js
@@ -1,17 +1,32 @@
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
 
-        if(top >= offset && top < offset + height) {
-            sec.classList.add('show-animate');
+//         if(top >= offset && top < offset + height) {
+//             sec.classList.add('show-animate');
+//         }
+//         else{
+//             sec.classList.remove('show-animate');
+//         }
+
+//     })
+// }
+
+// let sections = document.querySelectorAll('animate');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
         }
         else{
-            sec.classList.remove('show-animate');
+            entry.target.classList.remove('show');
         }
+    });
+});
 
-    })
-}
-
-let sections = document.querySelectorAll('section');
+const hiddenElements = document.querySelectorAll('.animate');
+hiddenElements.forEach((el) => observer.observe(el));
