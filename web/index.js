2c1ec5902d7d0583327773ec3e39e8464b272bfa
williamw04 <williamwu120@gmail.com>
2023-11-28 17:09:48 -0500

    Smooth Scrolling added, site layout tweaked, and further tweaks for Nav Bar

---
+++ web/index.js
@@ -0,0 +1,18 @@
+import Scrollbar from 'smooth-scrollbar';
+
+Scrollbar.init(document.querySelector('#my-scrollbar'));
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.animate');
+hiddenElements.forEach((el) => observer.observe(el));
