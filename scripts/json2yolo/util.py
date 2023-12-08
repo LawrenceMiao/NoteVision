2ce93713a781db1bb174895deb2d4e9a1158b1e0
kom5 <0120mkk@gmail.com>
2023-12-08 18:20:29 -0500

    more util functions

--- scripts/json2yolo/util.py
+++ scripts/json2yolo/util.py
@@ -28,3 +28,30 @@ def exif_size(img):
         pass
 
     return s
+
+
+def split_rows_simple(file='../data/sm4/out.txt'):  # from utils import *; split_rows_simple()
+    # splits one textfile into 3 smaller ones based upon train, test, val ratios
+    with open(file) as f:
+        lines = f.readlines()
+
+    s = Path(file).suffix
+    lines = sorted(list(filter(lambda x: len(x) > 0, lines)))
+    i, j, k = split_indices(lines, train=0.9, test=0.1, validate=0.0)
+    for k, v in {'train': i, 'test': j, 'val': k}.items():  # key, value pairs
+        if v.any():
+            new_file = file.replace(s, f'_{k}{s}')
+            with open(new_file, 'w') as f:
+                f.writelines([lines[i] for i in v])
+
+
+def split_files(out_path, file_name, prefix_path=''):  # split training data
+    file_name = list(filter(lambda x: len(x) > 0, file_name))
+    file_name = sorted(file_name)
+    i, j, k = split_indices(file_name, train=0.9, test=0.1, validate=0.0)
+    datasets = {'train': i, 'test': j, 'val': k}
+    for key, item in datasets.items():
+        if item.any():
+            with open(f'{out_path}_{key}.txt', 'a') as file:
+                for i in item:
+                    file.write('%s%s\n' % (prefix_path, file_name[i]))
