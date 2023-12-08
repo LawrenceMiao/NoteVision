c594205da974f01fcb26a7eb6f2c9c70dd6cdace
kom5 <0120mkk@gmail.com>
2023-12-08 18:23:23 -0500

    finished util.py

--- scripts/json2yolo/util.py
+++ scripts/json2yolo/util.py
@@ -123,3 +123,44 @@ def add_coco_background(path='../data/sm4/', n=1000):  # from utils import *; ad
 def create_single_class_dataset(path='../data/sm3'):  # from utils import *; create_single_class_dataset('../data/sm3/')
     # creates a single-class version of an existing dataset
     os.system(f'mkdir {path}_1cls')
+
+
+def flatten_recursive_folders(path='../../Downloads/data/sm4/'):  # from utils import *; flatten_recursive_folders()
+    # flattens nested folders in path/images and path/JSON into single folders
+    idir, jdir = f'{path}images/', f'{path}json/'
+    nidir, njdir = Path(f'{path}images_flat/'), Path(f'{path}json_flat/')
+    n = 0
+
+    # Create output folders
+    for p in [nidir, njdir]:
+        if os.path.exists(p):
+            shutil.rmtree(p)  # delete output folder
+        os.makedirs(p)  # make new output folder
+
+    for parent, dirs, files in os.walk(idir):
+        for f in tqdm(files, desc=parent):
+            f = Path(f)
+            stem, suffix = f.stem, f.suffix
+            if suffix.lower()[1:] in img_formats:
+                n += 1
+                stem_new = '%g_' % n + stem
+                image_new = nidir / (stem_new + suffix)  # converts all formats to *.jpg
+                json_new = njdir / f'{stem_new}.json'
+
+                image = parent / f
+                json = Path(parent.replace('images', 'json')) / str(f).replace(suffix, '.json')
+
+                os.system("cp '%s' '%s'" % (json, json_new))
+                os.system("cp '%s' '%s'" % (image, image_new))
+                # cv2.imwrite(str(image_new), cv2.imread(str(image)))
+
+    print('Flattening complete: %g jsons and images' % n)
+
+
+def coco91_to_coco80_class():  # converts 80-index (val2014) to 91-index (paper)
+    # https://tech.amikelive.com/node-718/what-object-categories-labels-are-in-coco-dataset/
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, None, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, None, 24, 25, None,
+         None, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, None, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+         51, 52, 53, 54, 55, 56, 57, 58, 59, None, 60, None, None, 61, None, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,
+         None, 73, 74, 75, 76, 77, 78, 79, None]
+    return x
