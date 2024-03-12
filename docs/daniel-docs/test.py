3c001941c4f0a00cd2847d96f4f5f240e6cbaaa8
danielshid <danielshi74@gmail.com>
2024-03-11 23:32:04 -0400

    visualizer test (not complete)

--- docs/daniel-docs/test.py
+++ docs/daniel-docs/test.py
@@ -1,11 +1,27 @@
 import mido
+from PIL import Image, ImageDraw
 
-# Define the path to your MIDI file
 midi_file_path = "C:/Users/danie/Documents/courses/S24/rcos/NoteVision/rach.mid"
-
-# Open the MIDI file
 midi_file = mido.MidiFile(midi_file_path)
 
 # Iterate through the MIDI file and print out the messages
 for msg in midi_file:
     print(msg)
+
+pic_width = 800
+pic_height = 600
+bg_color = (255, 255, 255)
+note_color = (0, 0, 0)
+
+image = Image.new("RGB", (pic_width, pic_height), bg_color)
+draw = ImageDraw.Draw(image)
+
+for i in midi_file:
+    if i.type == "note_on":
+        x = i.time * 10
+        y = i.note * 2
+        width = 10
+        height = 10
+        draw.rectangle([x, y, x + width, y + height], fill = note_color)
+
+image.show()
