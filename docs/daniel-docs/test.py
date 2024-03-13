3c89cf5e572c59b0ac108e710e24f27765bac7f3
danielshid <danielshi74@gmail.com>
2024-03-12 21:36:44 -0400

    visualizer progress

--- docs/daniel-docs/test.py
+++ docs/daniel-docs/test.py
@@ -1,27 +1,44 @@
-import mido
+from mido import MidiFile
 from PIL import Image, ImageDraw
 
-midi_file_path = "C:/Users/danie/Documents/courses/S24/rcos/NoteVision/rach.mid"
-midi_file = mido.MidiFile(midi_file_path)
+file_path = "C:/Users/danie/Documents/courses/S24/rcos/NoteVision/rach.mid"
+file = MidiFile(file_path)
 
-# Iterate through the MIDI file and print out the messages
-for msg in midi_file:
-    print(msg)
+for line in file:
+    print(line)
 
-pic_width = 800
-pic_height = 600
+max_note = 0
+max_time = 0
+for line in file:
+    if line.type == "note_on":
+        if line.note > max_note:
+            max_note = line.note
+        if line.time > max_time:
+            max_time = line.time
+
+pic_width = int(max_time * 300) + 500
+pic_height = (max_note + 1) * 10 + 250
+
 bg_color = (255, 255, 255)
-note_color = (0, 0, 0)
+note_color = (0, 0, 255)
+vertical_spacing = 7
 
 image = Image.new("RGB", (pic_width, pic_height), bg_color)
 draw = ImageDraw.Draw(image)
 
-for i in midi_file:
-    if i.type == "note_on":
-        x = i.time * 10
-        y = i.note * 2
+note_positions = {}
+
+file = MidiFile(file_path)
+
+for line in file:
+    if line.type == "note_on":
+        if line.note not in note_positions:
+            note_positions[line.note] = 0
+        x = line.time * 300
+        y = note_positions[line.note]
         width = 10
         height = 10
         draw.rectangle([x, y, x + width, y + height], fill = note_color)
+        note_positions[line.note] += vertical_spacing
 
 image.show()
