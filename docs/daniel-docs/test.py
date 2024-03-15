01f20508acd247e3b110d1f5d125d9089575a0c1
danielshid <danielshi74@gmail.com>
2024-03-15 17:16:12 -0400

    new mid and visual changes

--- docs/daniel-docs/test.py
+++ docs/daniel-docs/test.py
@@ -1,45 +1,53 @@
 from mido import MidiFile
 from PIL import Image, ImageDraw
 
-mid_file_path = "C:/Users/danie/Documents/courses/S24/rcos/NoteVision/rach.mid"
-mid_file = MidiFile(mid_file_path)
+def parse_mid(mid_path):
+    mid = MidiFile(mid_path)
+    file = open("for_elise_by_beethoven.txt", "w")
 
-max_note = 0
-max_time = 0
-for line in mid_file:
-    if line.type == "note_on":
-        if line.note > max_note:
-            max_note = line.note
-        if line.time > max_time:
-            max_time = line.time
+    total_time = 0
+    max_note = 0
+    for line in mid:
+        file.write(str(line) + "\n")
+        try:
+            total_time += line.time
+            if line.note > max_note:
+                max_note = line.note
+        except:
+            pass
+    file.close()
 
-pic_width = int(max_time * 350) + 100
-pic_height = (max_note * 15) + 300
+    return max_note, total_time
 
-bg_color = (255, 255, 255)
-note_color = (0, 0, 255)
-vertical_spacing = 5
+def make_img(mid, max_note, total_time):
+    img_color = (255, 255, 255)
+    note_color = (0, 0, 255)
 
-image = Image.new("RGB", (pic_width, pic_height), bg_color)
-draw = ImageDraw.Draw(image)
+    img = Image.new("RGB", (total_time, max_note), img_color)
+    draw = ImageDraw.Draw(img)
 
-note_positions = {}
-note_times = {}
+    note_positions = {}
+    note_times = {}
 
-mid_file = MidiFile(mid_file_path)
+    mid = MidiFile(mid_path)
 
-for line in mid_file:
-    if line.type == "note_on":
-        if line.note not in note_positions:
-            note_positions[line.note] = 0
-            note_times[line.note] = line.time
-        else:
-            note_positions[line.note] = note_times[line.note] * 100
-        x = note_positions[line.note]
-        y = line.note * 15
-        width = 4
-        height = 2
-        draw.rectangle([x, y, x + width, y + height], fill = note_color)
-        note_times[line.note] += line.time
+    for line in mid:
+        if line.type == "note_on":
+            if line.note not in note_positions:
+                note_positions[line.note] = 0
+                note_times[line.note] = line.time
+            else:
+                note_positions[line.note] = note_times[line.note] * 100
+            x = note_positions[line.note]
+            y = line.note * 15
+            width = 4
+            height = 2
+            draw.rectangle([x, y, x + width, y + height], fill = note_color)
+            note_times[line.note] += line.time
 
-image.show()
+    img.show()
+
+if __name__ == '__main__':
+    mid_path = 'C:/Users/danie/Documents/courses/S24/rcos/NoteVision/for_elise_by_beethoven.mid'
+    max_note, total_time = parse_mid(mid_path)
+    # make_img(mid_path, max_note, total_time)
