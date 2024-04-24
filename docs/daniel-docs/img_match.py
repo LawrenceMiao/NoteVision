083bb2fa2ef2b258fb7d1fb2650bd1598bd468ae
danielshid <danielshi74@gmail.com>
2024-04-23 21:09:37 -0400

    img_match properly displays larger images via resizeImage

--- docs/daniel-docs/img_match.py
+++ docs/daniel-docs/img_match.py
@@ -1,9 +1,34 @@
 import cv2
 
-def imgMatch(templatePath, imagePath):
+def resizeImage(img, maxWidth, maxHeight):
+    # Get original image dimensions
+    height, width = img.shape[:2]
+
+    # Calculate aspect ratio
+    aspectRatio = width / height
+
+    # Resize based on the larger dimension
+    if width > height:
+        newWidth = min(width, maxWidth)
+        newHeight = int(newWidth / aspectRatio)
+    else:
+        newHeight = min(height, maxHeight)
+        newWidth = int(newHeight * aspectRatio)
+
+    # Perform resizing
+    resizedImg = cv2.resize(img, (newWidth, newHeight), interpolation=cv2.INTER_AREA)
+
+    return resizedImg
+
+def imgMatch(templatePath, imagePath, maxDisplayWidth = 800, maxDisplayHeight = 600):
     # Read template and target image
     template = cv2.imread(templatePath)
     target = cv2.imread(imagePath)
+
+    # Resize images if they are larger than maximum display size
+    if max(template.shape[0], template.shape[1], target.shape[0], target.shape[1]) > maxDisplayWidth or max(template.shape[0], template.shape[1], target.shape[0], target.shape[1]) > maxDisplayHeight:
+        template = resizeImage(template, maxDisplayWidth, maxDisplayHeight)
+        target = resizeImage(target, maxDisplayWidth, maxDisplayHeight)
 
     # Convert images to grayscale
     templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -12,7 +37,7 @@ def imgMatch(templatePath, imagePath):
     # Perform template matching
     result = cv2.matchTemplate(targetGray, templateGray, cv2.TM_CCOEFF_NORMED)
 
-    # Get the location of the best match (minVal, maxVal, and minLoc are filler values)
+    # Get the location of the best match (minVal, maxVal, and minLoc are filler vars)
     minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
     matchLoc = maxLoc
 
