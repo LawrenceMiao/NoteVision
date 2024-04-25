99051a9022224e27cb15124425b26e0f1f2dd7fe
Lawrence M <lawrencejunzh@gmail.com>
2024-04-24 23:02:29 -0400

    superimpose edge

--- mock_noise/music_noise.py
+++ mock_noise/music_noise.py
@@ -2,6 +2,13 @@ import cv2
 import random
 
 
+def resize(img, scale):
+    new_width = img.shape[0] * scale
+    new_height = img.shape[1] * scale
+
+    return cv2.resize(image, (new_width, new_height))
+
+
 def rotate(img, angle):
     angle = 20
 
@@ -13,10 +20,16 @@ def rotate(img, angle):
 
 
 image = cv2.imread("sample1.png", cv2.IMREAD_GRAYSCALE)
+# image = resize(image, 2)
 
-_, binary_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+_, binary_image = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
 
-cv2.imshow("Binary Image", binary_image)
+superimposed_image = cv2.bitwise_or(binary_image, image)
+
+
+edges = cv2.Canny(binary_image, 100, 200)
+# cv2.imshow("Binary Image", binary_image)
+cv2.imshow("Detected Edges", superimposed_image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
